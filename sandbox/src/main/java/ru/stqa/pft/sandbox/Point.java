1773986ac26348e27023a838307b3ed78c5f77f2
AlexGraf <lexraf@list.ru>
2016-07-22 12:27:09 +0300

    Решение пункта 1.4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,9 +6,15 @@ public class Point {
   public double x;
   public double y;
 
+
   public Point(double x, double y) {
     this.x = x;
     this.y = y;
 
+  }
+
+  public double distance(Point p) {
+
+    return Math.sqrt((Math.pow((x - p.x), 2) + Math.pow((y - p.y), 2)));
   }
 }
