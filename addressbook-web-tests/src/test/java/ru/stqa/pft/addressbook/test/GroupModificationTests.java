2ca18ddfce0d394f90fd567004660a2c58afaabd
AlexGraf <lexraf@list.ru>
2016-08-10 23:31:15 +0300

    Добавление проверок на колличество элементов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
@@ -1,14 +1,16 @@
 package ru.stqa.pft.addressbook.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupModificationTests extends TestBase {
 
   @Test
-  public void testModificationGroup(){
+  public void testModificationGroup() {
     app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()){
+    int before = app.getContactHelper().getGroupCount();
+    if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
     app.getGroupHelper().selectGroup();
@@ -16,5 +18,7 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("Test78", "Test67", "Test65"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getContactHelper().getGroupCount();
+    Assert.assertEquals(after, before);
   }
 }
