fc3b9f3e58096c93e14bf8dc56eae45e4eec2af8
AlexGraf <lexraf@list.ru>
2016-08-17 23:36:15 +0300

    Fluent stile for GroupTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
@@ -1,19 +1,17 @@
 package ru.stqa.pft.addressbook.test;
-
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
-import java.util.Comparator;
-import java.util.List;
+import ru.stqa.pft.addressbook.model.Groups;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().size() == 0) {
+    if (app.group().all().size() == 0) {
       app.group().create(new GroupData()
               .withName("test1").withFooter("test2").withHeader("test3"));
     }
@@ -21,20 +19,15 @@ public class GroupModificationTests extends TestBase {
 
   @Test
   public void testModificationGroup() {
-    List<GroupData> before = app.group().list();
-    int index = before.size() - 1;
+    Groups before = app.group().all();
+    GroupData modifyGroup = before.iterator().next();
     GroupData group = new GroupData()
-            .withId(before.get(index).getId()).withName("test1").withHeader("Test67").withFooter("Test65");
-    app.group().modify(index, group);
-    List<GroupData> after = app.group().list();
-    Assert.assertEquals(after.size(), before.size());
+            .withId(modifyGroup.getId()).withName("test1").withHeader("Test67").withFooter("Test65");
+    app.group().modify(group);
+    Groups after = app.group().all();
+    assertThat(after.size(),equalTo(before.size()));
+    equalTo(before.without(modifyGroup).withAdded(group));
 
-    before.remove(index);
-    before.add(group);
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
-    Assert.assertEquals(before, after);
   }
 
 
