5e67ffaee935216275b16ef4a653c101ec6a6ac6
AlexGraf <lexraf@list.ru>
2016-09-18 22:57:43 +0300

    Интерграция с баг трекером Bugify

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
@@ -1,4 +1,33 @@
 package ru.stqa.pft.rest.tests;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.testng.SkipException;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class TestBase {
+  public boolean isIssueOpen(int issueId) throws IOException {
+    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues/" + issueId + ".json"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement status = parsed.getAsJsonObject().get("state_name");
+    String status = "close";
+    if(Objects.equals(status, "close") | Objects.equals(status, "resolved")){
+      return false;
+    }
+    return true;
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
+  }
 }
